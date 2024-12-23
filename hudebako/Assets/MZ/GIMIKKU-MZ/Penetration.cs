18982c593c8d57f0c40291622d55ae7ec5d2f0db
I-MZ <h1202415026@hiratagakuen.onmicrosoft.com>
2024-12-23 10:43:20 +0900

    関数名の変更＋α

--- hudebako/Assets/MZ/GIMIKKU-MZ/Penetration.cs
+++ hudebako/Assets/MZ/GIMIKKU-MZ/Penetration.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SURINUKEYUKA : MonoBehaviour
+public class Penetration : MonoBehaviour
 {
     public GameObject PlayerObject;
     PlayerController Pcnt;
