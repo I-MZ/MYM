3f3d608c7fbfb5cb5794e77262fc380e3df1acb4
I-MZ <h1202415026@hiratagakuen.onmicrosoft.com>
2025-01-27 09:49:41 +0900

    ステージ９の改善＋ステージクリア画面の変更

--- hudebako/Assets/MZ/GIMIKKU-MZ/Penetration.cs
+++ hudebako/Assets/MZ/GIMIKKU-MZ/Penetration.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//定規
 public class Penetration : MonoBehaviour
 {
     public GameObject PlayerObject;
@@ -18,43 +19,51 @@ public class Penetration : MonoBehaviour
     string nowanime = "";
     string oldanime = "";
 
+    //スタート関数
+    //説明
     // Start is called before the first frame update
     void Start()
     {
-        Pcnt = PlayerObject.GetComponent<PlayerController>();
-        BC = this.GetComponent<BoxCollider2D>();
+        //コンポーネント取得
+        Pcnt     = PlayerObject.GetComponent<PlayerController>();
+        BC       = this.GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //定規の開閉
         if (CkopenGravity == Pcnt.gravity) 
         {
             Debug.Log("開く？");
+            
+            //開ける
             if (nowanime != open)
             {
                 Debug.Log("開いた");
             }
 
-            BC.enabled = false;
-            nowanime = open;
+            BC.enabled = false;//当たり判定を無効化する
+            nowanime   = open; //開けるアニメーションに変更
         }
         else if(CkcloseGravity==Pcnt.gravity||PlayerController.gameState=="respawn")
         {
-            if (nowanime != close)
+
+            if (nowanime != close)//閉める
             {
                 Debug.Log("閉じた");
             }
 
-            BC.enabled = true;
-            nowanime = close;
+            BC.enabled = true; //当たり判定を有効化する
+            nowanime   = close;//閉めるアニメーションに変更
         }
 
-        if (nowanime != oldanime)
+        //アニメーションの変更の処理
+        if (nowanime != oldanime)   //アニメーションが同じか確認する
         {
-            oldanime = nowanime;
-            animator.Play(nowanime);
+            oldanime = nowanime;    //oldanimeに新しいアニメーションを設定する
+            animator.Play(nowanime);//nowanimeに入っているアニメーションを流す
             Debug.Log("anime = " + nowanime);
         }
     }
